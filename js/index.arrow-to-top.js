ee92db5379c7ad1f7ffd1939f04750317fbada6a
Kaznokh <113619266+MegaKonzo@users.noreply.github.com>
2025-01-26 12:47:06 +0000

    add section tournaments and button ToTop

---
+++ js/index.arrow-to-top.js
@@ -0,0 +1,17 @@
+const backToTopButton = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        backToTopButton.classList.add("show");
+      } else {
+        backToTopButton.classList.remove("show");
+      }
+    });
+    
+document.getElementById("backToTop").addEventListener("click", (event) => {
+  event.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
