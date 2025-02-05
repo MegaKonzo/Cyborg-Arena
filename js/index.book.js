e83b97487edb0762468a6ddc8a4b600166a235a2
MEGAKONZO <foroforo94@gmail.com>
2025-02-05 20:14:38 +0200

    book continue...

--- js/index.book.js
+++ js/index.book.js
@@ -1,7 +1,9 @@
-document.getElementById("book-now-button").addEventListener("click", function () {
-  document.getElementById("booking-modal").style.display = "block";
-  document.body.classList.add("overflow-hidden"); // Заборона прокручування
-});
+document
+  .getElementById("book-now-button")
+  .addEventListener("click", function () {
+    document.getElementById("booking-modal").style.display = "block";
+    document.body.classList.add("overflow-hidden"); // Заборона прокручування
+  });
 
 document.querySelector(".close").addEventListener("click", function () {
   document.getElementById("booking-modal").style.display = "none";
@@ -14,3 +16,31 @@ window.addEventListener("click", function (event) {
     document.body.classList.remove("overflow-hidden");
   }
 });
+
+
+
+
+
+
+
+
+//const hall = document.getElementById("main-hall");
+//const seats = 40;
+//const rows = 4;
+//const cols = 10;
+//const reservedSeats = new Set([5, 12, 20]); 
+
+//for (let i = 1; i <= seats; i++) {
+  //  const seat = document.createElement("div");
+    //seat.classList.add("seat");
+    //seat.textContent = i;
+    
+    //if (reservedSeats.has(i)) {
+      //  seat.classList.add("reserved");
+    //} else {
+      //  seat.addEventListener("click", () => {
+        //    alert(`Ви обрали місце №${i}. Виберіть час бронювання.`);
+        //});
+    //}
+    //hall.appendChild(seat);
+//}
